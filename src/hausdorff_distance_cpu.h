eba35b4506a5cc770b98f939009c998441b1549d
Liang Geng <geng.161@osu.edu>
2025-01-16 15:52:20 -0500

    cleanup

--- src/hausdorff_distance_cpu.h
+++ src/hausdorff_distance_cpu.h
@@ -12,40 +12,7 @@
 #include "utils/type_traits.h"
 
 namespace hd {
-
-template <typename POINT_T>
-typename vec_info<POINT_T>::type CalculateHausdorffDistance(
-    std::vector<POINT_T>& points_a, std::vector<POINT_T>& points_b) {
-  using coord_t = typename vec_info<POINT_T>::type;
-  coord_t cmax = 0;
-  std::random_device rd;  // Seed source
-  std::mt19937 g(rd());   // Mersenne Twister engine seeded with rd()
-
-  // Shuffle the vector
-  std::shuffle(points_a.begin(), points_a.end(), g);
-  std::shuffle(points_b.begin(), points_b.end(), g);
-  uint32_t compared_pairs = 0;
-
-  for (size_t i = 0; i < points_a.size(); i++) {
-    coord_t cmin = std::numeric_limits<coord_t>::max();
-
-    for (size_t j = 0; j < points_b.size(); j++) {
-      auto d = EuclideanDistance2(points_a[i], points_b[j]);
-      if (d < cmin) {
-        cmin = d;
-      }
-      compared_pairs++;
-      if (cmin <= cmax) {
-        break;
-      }
-    }
-    if (cmin != std::numeric_limits<coord_t>::max() && cmin > cmax) {
-      cmax = cmin;
-    }
-  }
-  LOG(INFO) << "Compared Pairs: " << compared_pairs;
-  return sqrt(cmax);
-}
+namespace detail {
 
 DEV_HOST_INLINE
 std::uint32_t expand_bits(std::uint32_t v) {
@@ -96,13 +63,109 @@ uint32_t morton_code(double3 xyz, float resolution = 1024.0f) {
   return xx * 4 + yy * 2 + zz;
 }
 
+template <typename T>
+void update_maximum(std::atomic<T>& maximum_value, T const& value) noexcept {
+  T prev_value = maximum_value;
+  while (prev_value < value &&
+         !maximum_value.compare_exchange_weak(prev_value, value)) {}
+}
+}  // namespace detail
+
+template <typename POINT_T>
+typename vec_info<POINT_T>::type CalculateHausdorffDistance(
+    std::vector<POINT_T>& points_a, std::vector<POINT_T>& points_b) {
+  using coord_t = typename vec_info<POINT_T>::type;
+  coord_t cmax = 0;
+  std::random_device rd;  // Seed source
+  std::mt19937 g(rd());   // Mersenne Twister engine seeded with rd()
+
+  // Shuffle the vector
+  std::shuffle(points_a.begin(), points_a.end(), g);
+  std::shuffle(points_b.begin(), points_b.end(), g);
+  uint32_t compared_pairs = 0;
+
+  for (size_t i = 0; i < points_a.size(); i++) {
+    coord_t cmin = std::numeric_limits<coord_t>::max();
+
+    for (size_t j = 0; j < points_b.size(); j++) {
+      auto d = EuclideanDistance2(points_a[i], points_b[j]);
+      if (d < cmin) {
+        cmin = d;
+      }
+      compared_pairs++;
+      if (cmin <= cmax) {
+        break;
+      }
+    }
+    if (cmin != std::numeric_limits<coord_t>::max() && cmin > cmax) {
+      cmax = cmin;
+    }
+  }
+  LOG(INFO) << "Compared Pairs: " << compared_pairs;
+  return sqrt(cmax);
+}
+
+
+
+template <typename POINT_T>
+typename vec_info<POINT_T>::type CalculateHausdorffDistanceParallel(
+    std::vector<POINT_T>& points_a, std::vector<POINT_T>& points_b) {
+  using coord_t = typename vec_info<POINT_T>::type;
+  std::random_device rd;  // Seed source
+  std::mt19937 g(rd());   // Mersenne Twister engine seeded with rd()
+
+  // Shuffle the vector
+  std::shuffle(points_a.begin(), points_a.end(), g);
+  std::shuffle(points_b.begin(), points_b.end(), g);
+
+  std::vector<std::thread> threads;
+  auto thread_count = std::thread::hardware_concurrency();
+  auto avg_points = (points_a.size() + thread_count - 1) / thread_count;
+  std::atomic<coord_t> cmax;
+  std::atomic_uint64_t compared_pairs{0};
+
+  cmax = 0;
+
+  for (int tid = 0; tid < thread_count; tid++) {
+    threads.emplace_back(std::thread([&, tid]() {
+      auto begin = tid * avg_points;
+      auto end = std::min(begin + avg_points, points_a.size());
+      uint64_t local_compared_pairs = 0;
+
+      for (int i = begin; i < end; i++) {
+        auto cmin = std::numeric_limits<coord_t>::max();
+        for (size_t j = 0; j < points_b.size(); j++) {
+          auto d = EuclideanDistance2(points_a[i], points_b[j]);
+          local_compared_pairs++;
+          if (d < cmin) {
+            cmin = d;
+          }
+          if (cmin < cmax) {
+            break;
+          }
+        }
+        if (cmin != std::numeric_limits<coord_t>::max()) {
+          detail::update_maximum(cmax, cmin);
+        }
+      }
+      compared_pairs += local_compared_pairs;
+    }));
+  }
+
+  for (auto& thread : threads) {
+    thread.join();
+  }
+  LOG(INFO) << "Compared Pairs: " << compared_pairs;
+  return sqrt(cmax);
+}
+
 template <typename POINT_T>
 typename vec_info<POINT_T>::type CalculateHausdorffDistanceZOrder(
     std::vector<POINT_T>& points_a, std::vector<POINT_T>& points_b) {
   using coord_t = typename vec_info<POINT_T>::type;
   coord_t cmax = 0;
   auto comp = [](const POINT_T& a, const POINT_T& b) {
-    return morton_code(a) < morton_code(b);
+    return detail::morton_code(a) < detail::morton_code(b);
   };
   std::sort(points_a.begin(), points_a.end(), comp);
   std::sort(points_b.begin(), points_b.end(), comp);
@@ -148,7 +211,7 @@ typename vec_info<POINT_T>::type CalculateHausdorffDistanceYuan(
   using coord_t = typename vec_info<POINT_T>::type;
   coord_t cmax = 0;
   auto comp = [](const POINT_T& a, const POINT_T& b) {
-    return morton_code(a) < morton_code(b);
+    return detail::morton_code(a) < detail::morton_code(b);
   };
   std::sort(points_a.begin(), points_a.end(), comp);
   std::sort(points_b.begin(), points_b.end(), comp);
@@ -160,7 +223,7 @@ typename vec_info<POINT_T>::type CalculateHausdorffDistanceYuan(
       auto& p_b = points_b[i];
       auto& p_a = points_a[j];
 
-      if (morton_code(p_b) >= morton_code(p_a)) {
+      if (detail::morton_code(p_b) >= detail::morton_code(p_a)) {
         Aloc[j] = i;
         j++;
         i--;
@@ -240,63 +303,5 @@ typename vec_info<POINT_T>::type CalculateHausdorffDistanceYuan(
   return sqrt(cmax);
 }
 
-template <typename T>
-void update_maximum(std::atomic<T>& maximum_value, T const& value) noexcept {
-  T prev_value = maximum_value;
-  while (prev_value < value &&
-         !maximum_value.compare_exchange_weak(prev_value, value)) {}
-}
-
-template <typename POINT_T>
-typename vec_info<POINT_T>::type CalculateHausdorffDistanceParallel(
-    std::vector<POINT_T>& points_a, std::vector<POINT_T>& points_b) {
-  using coord_t = typename vec_info<POINT_T>::type;
-  std::random_device rd;  // Seed source
-  std::mt19937 g(rd());   // Mersenne Twister engine seeded with rd()
-
-  // Shuffle the vector
-  std::shuffle(points_a.begin(), points_a.end(), g);
-  std::shuffle(points_b.begin(), points_b.end(), g);
-
-  std::vector<std::thread> threads;
-  auto thread_count = std::thread::hardware_concurrency();
-  auto avg_points = (points_a.size() + thread_count - 1) / thread_count;
-  std::atomic<coord_t> cmax;
-  std::atomic_uint64_t compared_pairs{0};
-
-  cmax = 0;
-
-  for (int tid = 0; tid < thread_count; tid++) {
-    threads.emplace_back(std::thread([&, tid]() {
-      auto begin = tid * avg_points;
-      auto end = std::min(begin + avg_points, points_a.size());
-      uint64_t local_compared_pairs = 0;
-
-      for (int i = begin; i < end; i++) {
-        auto cmin = std::numeric_limits<coord_t>::max();
-        for (size_t j = 0; j < points_b.size(); j++) {
-          auto d = EuclideanDistance2(points_a[i], points_b[j]);
-          local_compared_pairs++;
-          if (d < cmin) {
-            cmin = d;
-          }
-          if (cmin < cmax) {
-            break;
-          }
-        }
-        if (cmin != std::numeric_limits<coord_t>::max()) {
-          update_maximum(cmax, cmin);
-        }
-      }
-      compared_pairs += local_compared_pairs;
-    }));
-  }
-
-  for (auto& thread : threads) {
-    thread.join();
-  }
-  LOG(INFO) << "Compared Pairs: " << compared_pairs;
-  return sqrt(cmax);
-}
 }  // namespace hd
 #endif  // HAUSDORFF_DISTANCE_CPU_H
